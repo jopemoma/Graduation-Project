ccaa128769cea4080d97736c61d4361ad29d4a1d
bragerosberg <brage.rosberg@salt.dev>
2020-07-21 13:23:40 +0200

    Working on facebook login

--- server/server.js
+++ server/server.js
@@ -6,10 +6,24 @@ const password = process.env.PASSWORD;
 console.log(userName, password);
 
 
-mongoose.connect(`mongodb+srv://${userName}:${password}@cluster0.1oydg.mongodb.net/Cluster0?retryWrites=true&w=majority`, { useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://${userName}:${password}@cluster0.1oydg.mongodb.net/BidraSammen?retryWrites=true&w=majority`, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connecteed!');
 });
+
+const userSchema = new mongoose.Schema({
+  name: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+const martin = new User({ name: 'Martin' });
+
+martin.save(function (err) {
+  if (err) return console.error(err);
+});
+
+
